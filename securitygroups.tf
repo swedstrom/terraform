88b40e0c903861f1b73372ce44b10441017ecfe5
Shawn <caleman@gmail.com>
2018-01-01 12:50:01 -0700

    staging up the new stack added SG

---
+++ securitygroups.tf
@@ -0,0 +1,95 @@
+resource "aws_security_group" "FrontEnd" {
+  name = "FrontEnd"
+  tags {
+        Name = "FrontEnd"
+  }
+  description = "ONLY HTTP CONNECTION INBOUD"
+  vpc_id = "${aws_vpc.terraformmain.id}"
+  tags {
+    Environment = "${var.stack}"
+   }
+
+  ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "TCP"
+    cidr_blocks = ["${var.ssh_ingress}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ELB security group to access
+# the ELB over HTTP
+resource "aws_security_group" "elb" {
+  name        = "elb_sg"
+  description = "Used in the terraform"
+  tags {
+    Environment = "${var.stack}"
+  }
+
+  vpc_id = "${aws_vpc.default.id}"
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ensure the VPC has an Internet gateway or this step will fail
+  depends_on = ["aws_internet_gateway.gw"]
+}
+
+resource "aws_security_group" "Database" {
+  name = "Database"
+  tags {
+        Name = "Database"
+        Environment = "${var.stack}"
+  }
+  description = "ONLY tcp CONNECTION INBOUND"
+  vpc_id = "${aws_vpc.terraformmain.id}"
+  ingress {
+      from_port = 3306
+      to_port = 3306
+      protocol = "TCP"
+      security_groups = ["${aws_security_group.FrontEnd.id}"]
+  }
+  ingress {
+      from_port   = "22"
+      to_port     = "22"
+      protocol    = "TCP"
+      cidr_blocks = ["${var.Subnet-Private-AzA-CIDR}"]
+  }
+ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
