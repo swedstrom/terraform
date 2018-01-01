f2cb395057a56db8ae3570900bcccf55a9ab3dd4
Shawn <caleman@gmail.com>
2018-01-01 13:00:28 -0700

    staging up the new stack added new tags for env

---
+++ elb.tf
@@ -0,0 +1,65 @@
+resource "aws_elb" "web" {
+  name = "example-elb"
+
+  # The same availability zone as our instance
+  subnets = ["${aws_subnet.tf_test_subnet.id}"]
+
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  # The instance is registered automatically
+
+  instances                   = ["${aws_instance.web.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+resource "aws_lb_cookie_stickiness_policy" "default" {
+  name                     = "lbpolicy"
+  load_balancer            = "${aws_elb.web.id}"
+  lb_port                  = 80
+  cookie_expiration_period = 600
+}
+
+resource "aws_instance" "web" {
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair you've created and downloaded
+  # from the AWS console.
+  #
+  # https://console.aws.amazon.com/ec2/v2/home?region=us-west-2#KeyPairs:
+  #
+  key_name = "${var.key_name}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  subnet_id              = "${aws_subnet.tf_test_subnet.id}"
+  user_data              = "${file("userdata.sh")}"
+
+  #Instance tags
+
+  tags {
+    Name = "elb-example"
+    
+  }
+}
